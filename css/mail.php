6db9ce5d335325f76b058ddb3622ce2c971e757d
Carlos Andrés <cavu93@hotmail.com>
2021-09-06 21:49:30 -0600

    correo nuevi

--- css/mail.php
+++ css/mail.php
@@ -1,20 +1,51 @@
 <?php
-
-if( isset($_POST['n']) && isset($_POST['e']) && isset($_POST['m']) ){
-	$n = $_POST['n']; // HINT: use preg_replace() to filter the data
-	$e = $_POST['e'];
-	$m = nl2br($_POST['m']);
-	$to = "cavaal93@gmmail.com";	
-	$from = $e;
-	$subject = 'Contact Form Message';
-	$message = '<b>Name:</b> '.$n.' <br><b>Email:</b> '.$e.' <p>'.$m.'</p>';
-	$headers = "From: $from\n";
-	$headers .= "MIME-Version: 1.0\n";
-	$headers .= "Content-type: text/html; charset=iso-8859-1\n";
-	if( mail($to, $subject, $message, $headers) ){
-		echo "success";
-	} else {
-		echo "The server failed to send the message. Please try again later.";
-	}
+// Proccess at only POST metheod
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // name of sender
+    $name = strip_tags(trim($_POST["name"]));
+    
+    // Email of sender
+    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    
+    // Sender subject
+    $subject = strip_tags(trim($_POST["subject"]));
+    
+    // Sender Message
+    $message = trim($_POST["message"]);
+    
+    // Your email where this email will be sent
+    $your_email = "cavaal93@gmail.com";
+    //Your site name for identify 
+    $your_site_name = "Example";
+    
+    // Build email subject
+    $email_subject = "[{$your_site_name}] New Message by {$name}";
+    
+    // Build Email Content
+    $email_content = "Name: {$name}\n";
+    $email_content .= "Email: {$email}\n";
+    $email_content .= "Subject: {$subject}\n";
+    $email_content .= "Message: {$message}\n";
+    
+    // Build email headers
+    $email_headers = "From: {$name} <{$email}>";
+    
+    // Send email
+    $send_email = mail($your_email, $email_subject, $email_content, $email_headers);
+    
+    // Check email sent or not
+    if($send_email){
+        // Send a 200 response code.
+        http_response_code(200);
+        echo "Thank You! Your message has been sent."; 
+    } else {
+        // Send a 500 response code.
+        http_response_code(500);
+        echo "Oops! we couldn't send your message. Please try again later";
+    }
+} else {
+    // Send 403 response code
+    http_response_code(403);
+    echo "Oops! Submition problem. Please try again later";
 }
 ?>
